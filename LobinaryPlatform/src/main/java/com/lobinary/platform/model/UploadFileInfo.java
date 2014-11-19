b2219ea737132bedc32d029aa473e05bf5ef91d9
lobinary <919515134@qq.com>
2014-11-19 16:19:12 +0800

    增加上传文件对象

---
+++ LobinaryPlatform/src/main/java/com/lobinary/platform/model/UploadFileInfo.java
@@ -0,0 +1,98 @@
+package com.lobinary.platform.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="RECEIVER_UPLOAD_FILE_INFO")
+public class UploadFileInfo implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3309298344570369971L;
+	
+	/**
+	 * ID
+	 */
+	private int id;
+	/**
+	 * 发送方
+	 */
+	private String sender;
+	/**
+	 * 接收方
+	 */
+	private String receiver;
+	/**
+	 * 文件种类
+	 */
+	private int fileType;
+	/**
+	 * 文件位置
+	 */
+	private String fileLocation;
+	/**
+	 * 发送时间
+	 */
+	private Date sendDate;
+	
+	
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public int getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(int fileType) {
+		this.fileType = fileType;
+	}
+
+	public String getFileLocation() {
+		return fileLocation;
+	}
+
+	public void setFileLocation(String fileLocation) {
+		this.fileLocation = fileLocation;
+	}
+
+	public Date getSendDate() {
+		return sendDate;
+	}
+
+	public void setSendDate(Date sendDate) {
+		this.sendDate = sendDate;
+	}
+
+}
