a97f983ec6b48cb69e7e9b6b6876397668dca078
Lobinary <Lobinary@Lobinary-PC>
2015-09-01 15:38:52 +0800

    PC-测试

---
+++ PC-TEST/src/main/java/com/lobinary/test/svn/SVNKitTest.java
@@ -0,0 +1,42 @@
+/*
+ * @(#)SVNKitTest.java     V1.0.0      @下午6:00:45
+ *
+ * Project: PC-TEST
+ *
+ * Modify Information:
+ *    Author        Date        Description
+ *    ============  ==========  =======================================
+ *    lvbin       	2015年8月28日    	Create this file
+ * 
+ * Copyright Notice:
+ *     Copyright (c) 2009-2014 Unicompay Co., Ltd. 
+ *     1002 Room, No. 133 North Street, Xi Dan, 
+ *     Xicheng District, Beijing ,100032, China 
+ *     All rights reserved.
+ *
+ *     This software is the confidential and proprietary information of
+ *     Unicompay Co., Ltd. ("Confidential Information").
+ *     You shall not disclose such Confidential Information and shall use 
+ *     it only in accordance with the terms of the license agreement you 
+ *     entered into with Unicompay.
+ */
+package com.lobinary.test.svn;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * @author 吕斌：lvb3@chinaunicom.cn
+ * @since 2015年8月28日 下午6:00:45
+ * @version V1.0.0 描述 : 创建文件SVNKitTest
+ * 
+ *         
+ * 
+ */
+public class SVNKitTest {
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
