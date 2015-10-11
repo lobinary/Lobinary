81c61ac65444ca08e9706cee76d01e34b3d656e4
lobinary <919515134@qq.com>
2015-10-11 22:17:34 +0800

    尝试 提交常量类

---
+++ ControlClinetPC/src/main/java/com/lobinary/apc/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.lobinary.apc.constants;
+
+/**
+ * 
+ *	<pre> 
+ *	基本常量类
+ * 	</pre>
+ * @author lobinary 
+ * @since 2015年10月11日 下午10:15:38
+ * @version V1.0.0 描述 : 创建该文件
+ *
+ */
+public class Constants {
+	
+	
+
+}
---
+++ ControlClinetPC/src/main/java/com/lobinary/apc/constants/EConstants.java
@@ -0,0 +1,15 @@
+package com.lobinary.apc.constants;
+
+/**
+ * 
+ *	<pre> 
+ *	异常常量类
+ * 	</pre>
+ * @author lobinary 
+ * @since 2015年10月11日 下午10:16:54
+ * @version V1.0.0 描述 : 创建该文件
+ *
+ */
+public class EConstants {
+
+}
