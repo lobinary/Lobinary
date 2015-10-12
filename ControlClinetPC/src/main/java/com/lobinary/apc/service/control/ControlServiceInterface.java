8d618594fb7d5894ad23205dff9fe7f2a7e00854
Lobinary <Lobinary@Lobinary-PC>
2015-10-12 13:48:40 +0800

    增加通信模块

---
+++ ControlClinetPC/src/main/java/com/lobinary/apc/service/control/ControlServiceInterface.java
@@ -0,0 +1,26 @@
+package com.lobinary.apc.service.control;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * @author 吕斌：lvb3@chinaunicom.cn
+ * @since 2015年10月12日 下午1:17:12
+ * @version V1.0.0 描述 : 创建文件ControlServiceInterface
+ * 
+ *         
+ * 
+ */
+public interface ControlServiceInterface {
+	
+	/**
+	 * 
+	 * <pre>
+	 * 关闭显示器
+	 * </pre>
+	 *
+	 * @return
+	 */
+	public boolean shutdownScreen();
+
+}
