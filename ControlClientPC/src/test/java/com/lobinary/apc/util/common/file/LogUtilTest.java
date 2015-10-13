cdb0ba31974317a56f972f26c0906c3e0ed44303
lobinary <919515134@qq.com>
2015-10-13 22:14:03 +0800

    增加配置路径

---
+++ ControlClientPC/src/test/java/com/lobinary/apc/util/common/file/LogUtilTest.java
@@ -0,0 +1,39 @@
+/*
+ * @(#)LogUtilTest.java     V1.0.0      @下午10:13:02
+ *
+ * 项目名称: ControlClientPC
+ *
+ * 更改 信息:
+ *    作者        				   日期        			描述
+ *    ============  	================  =======================================
+ *    lobinary       	  2015年10月13日    	创建文件
+ *
+ */
+package com.lobinary.apc.util.common.file;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * @author 吕斌：lvb3@chinaunicom.cn
+ * @since 2015年10月13日 下午10:13:02
+ * @version V1.0.0 描述 : 创建文件LogUtilTest
+ * 
+ *         
+ * 
+ */
+public class LogUtilTest {
+
+	/**
+	 * Test method for {@link com.lobinary.apc.util.common.file.LogUtil#getLogger(java.lang.Class)}.
+	 */
+	@Test
+	public void testGetLogger() {
+		fail("Not yet implemented");
+	}
+
+}
