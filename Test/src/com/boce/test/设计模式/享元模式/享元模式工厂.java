0b33f269f35ee519f19da733037e3f24e036bd41
Lobinary <Lobinary@919515134@qq.com>
2016-07-18 10:43:53 +0800

    新增设计模式相关代码

---
+++ Test/src/com/boce/test/设计模式/享元模式/享元模式工厂.java
@@ -0,0 +1,19 @@
+package com.boce.test.设计模式.享元模式;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 享元模式工厂 {
+	
+	private static Map<String,圆形> 圆形集合 = new HashMap<String,圆形>();
+	
+	public static 圆形 获取圆形(String 颜色){
+		圆形 圆形 = 圆形集合.get(颜色);
+		if(圆形==null){
+			圆形 = new 圆形(颜色);
+			圆形集合.put(颜色, 圆形);
+		}
+		return 圆形;
+	}
+
+}
